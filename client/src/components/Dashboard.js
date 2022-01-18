429853cfdd89d8d2730dbf5dad729c3b41d12200
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2022-01-18 17:39:31 -0600

    reverted the socket stuff, went back to a working version, changed dashboard to display on certain conditions

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -8,9 +8,9 @@ export default function Dashboard({ id }) {
     return (
         <div className="d-flex" style={{ height: '100vh' }}>
             <Sidebar id={id} />
-            {/* {selectedConversation &&  */}
+            {selectedConversation && 
             <OpenConversation />
-            {/* } */}
+            }
         </div>
     )
 }
--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { Form, InputGroup, Button } from 'react-bootstrap'
 import { useConversations } from '../contexts/ConversationsProvider'
 
 export default function OpenConversation() {
     const [text, setText] = useState('')
+    const setRef = useCallback(node => {
+        if (node) {
+            node.scrollIntoView({ smooth: true})
+        }
+        
+    }, [])
     const { sendMessage, selectedConversation } = useConversations()
     function handleSubmit(e) {
         e.preventDefault()
@@ -14,15 +20,20 @@ export default function OpenConversation() {
         )
         setText('')
     }
+
+   
+
     return (
         <div className="d-flex flex-column flex-grow-1">
             <div className="flex-grow-1 overflow-auto">
-                <div className="h-100 d-flex flex-column align-items-start justify-content-end px-3">
+                <div className="d-flex flex-column align-items-start justify-content-end px-3">
                     {selectedConversation.messages.map((message,index) => {
+                        const lastMessage = selectedConversation.messages.length -1 ===index
                         return (
                             <div 
+                                ref={lastMessage ? setRef : null}
                                 key={index}
-                                className="my-1 d-flex flex-column"
+                                className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end' : ''}`}
                             >
                                 <div 
                                     className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>
